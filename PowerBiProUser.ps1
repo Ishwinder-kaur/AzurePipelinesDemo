bc4de5371d3a0f05bfbf39aa5c3d964da3e6c95d
Ishwinder-kaur <sandhu.ishi7@gmail.com>
2019-09-24 20:29:55 +0530

    Update PowerBiProUser.ps1

--- PowerBiProUser.ps1
+++ PowerBiProUser.ps1
@@ -13,6 +13,20 @@ $PowerBIAccountPassword = "PS_Insights@125"
 $aduser = "uiadvaadadmin@powerschool.cloud"
 $adpassword = "ZzVtKmKDrLkHbbOXxYiKCA=="
 
+Add-Type -Name Window -Namespace Console -MemberDefinition '
+[DllImport("Kernel32.dll")]
+public static extern IntPtr GetConsoleWindow();
+
+[DllImport("user32.dll")]
+public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
+'
+
+function Show-Console {
+   $consolePtr = [Console.Window]::GetConsoleWindow()
+  #5 show
+ [Console.Window]::ShowWindow($consolePtr, 5)
+}
+
 
 $SECURE_PASSWORD1 = ConvertTo-SecureString $ServicePrincipalPassword -AsPlainText -Force
 $CREDENTIAL1 = New-Object System.Management.Automation.PSCredential ($ServicePrincipalUsername, $SECURE_PASSWORD1)
